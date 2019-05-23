6c31eb5c70017900ec437bf22a17e146226f9795
Sebastian Higuita <sebastianhc@kometsales.com>
2019-05-23 10:28:32 -0500

    flutter_material_components_sample

---
+++ README.md
@@ -0,0 +1,2 @@
+ # Flutter: Material Components (MDC) Basics (Flutter)
+[Codelab 101](https://codelabs.developers.google.com/codelabs/mdc-101-flutter/#1)
---
+++ lib/login.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          children: <Widget>[
+            SizedBox(height: 80),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/images/diamond.png'),
+                SizedBox(height: 16),
+                Text('SHRINE'),
+              ],
+            ),
+            SizedBox(height: 120),
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            SizedBox(height: 12),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child: Text('CANCEL'),
+                  onPressed: () {
+                    _usernameController.clear();
+                    _passwordController.clear();
+                  },
+                ),
+                RaisedButton(
+                  child: Text('NEXT'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
